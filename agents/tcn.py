e74baa0917055fb7ef33263d581644c954eed782
Park, Soo Hyung <spark639@gatech.edu>
2021-11-09 03:48:24 -0500

    created tcn script.

---
+++ agents/tcn.py
@@ -0,0 +1,37 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+import os
+from pathlib import Path
+import numpy as np
+import torch.optim as optim
+from torchvision.transforms.transforms import RandomApply
+from core.transforms import GaussianBlur, TwoCropsTransform
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader
+from agents.net import MLP, Encoder
+
+# ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_path', default='', type=str, help='image dataset folder to train model from')
+parser.add_argument('--batch_size', default=64, type=int, help='training batch size')
+parser.add_argument('--nb_epochs', default=20, type=int, help='number of epochs')
+parser.add_argument('--lr', default=1e-3, type=float, help='learning rate')
+parser.add_argument('--seed', default=102, type=int, help='random seed')
+args = parser.parse_args()
+
+if not os.path.exists('models/'):
+    os.makedirs('models/')
+MODEL_PATH = 'models/' + Path(args.data_path).parts[-1] + 'tcn.pt'
+
+random.seed(args.seed)
+torch.manual_seed(args.seed)
+np.random.seed(args.seed)
+
+# TODO: Continue from below...
