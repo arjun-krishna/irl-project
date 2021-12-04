c9f2320b2dc428ec143f8c82dfbdab7b9083a598
Arjun Krishna <arjunkris96@gmail.com>
2021-12-03 23:54:15 -0500

    add action history custom dataset and implement behavior clone that uses that dataset

---
+++ agents/bc_hist.py
@@ -0,0 +1,157 @@
+"""
+Action considered BC
+"""
+import argparse
+import pickle
+from numpy.core.defchararray import mod
+import torch
+import random
+import gym
+import gym_miniworld
+import os
+from pathlib import Path
+import numpy as np
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import torch.optim as optim
+from PIL import Image
+from agents.net import MLP, Encoder
+from agents.hist_dataset import DemoDataPreviousAction
+from core.model_metrics import ModelMetrics
+
+# ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--env-name', default='MiniWorld-Hallway-v0', type=str, help='env name to evaluate on')
+parser.add_argument('--top_view', action='store_true', help='show the top view instead of the agent view')
+parser.add_argument('--domain-rand', action='store_true', help='enable domain randomization')
+parser.add_argument('--data-path', default='', type=str, help='folder of demos')
+parser.add_argument('--nb-demos', default=50, type=int, help='number of demos to consider for training')
+parser.add_argument('--batch_size', default=64, type=int, help='training batch size')
+parser.add_argument('--nb_epochs', default=20, type=int, help='number of epochs')
+parser.add_argument('--eval_epoch', default=10, type=int, help='run evaluate after specified epochs')
+parser.add_argument('--lr', default=5e-3, type=float, help='learning rate')
+parser.add_argument('--seed', default=101, type=int, help='random seed')
+args = parser.parse_args()
+
+if not os.path.exists('models/'):
+    os.makedirs('models/')
+MODEL_NAME = 'D' + str(args.nb_demos) + '_bc_hist'
+MODEL_PATH = 'models/' + MODEL_NAME + '.pt'
+MODEL_METRICS_PATH = 'models/' + MODEL_NAME + '.pickle'
+
+random.seed(args.seed)
+torch.manual_seed(args.seed)
+np.random.seed(args.seed)
+
+normalizer = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                std=[0.229, 0.224, 0.225])
+
+data_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((50, 50)),
+    normalizer
+])
+
+train_dataset = DemoDataPreviousAction(args.data_path, args.nb_demos, data_transform)
+train_loader = DataLoader(train_dataset, args.batch_size, shuffle=True)
+
+class ModelWithHist(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.enc = Encoder(dim=128)
+        self.mlp = MLP(dim=137)
+    
+    def forward(self, sample):
+        e = self.enc(sample['obs'])
+        X = torch.concat([e, sample['prev_a']], dim=1)
+        return self.mlp(X)
+
+model = ModelWithHist()
+
+def evaluateInEnv():
+    env = gym.make(args.env_name)
+    env.reset()
+    get_obs = env.render_top_view if args.top_view else env.render_obs
+
+    if args.domain_rand:
+        env.domain_rand = True
+
+    NUM_EPISODES = 100
+
+    metric_steps = []
+    metric_success = []
+
+    for i in range(NUM_EPISODES):
+        done = False
+        steps = 0
+        prev_a = 8
+        while not done:
+            sample = {
+                'obs': data_transform(Image.fromarray(get_obs()))[np.newaxis, :, :, :],
+                'prev_a': F.one_hot(torch.tensor([prev_a]), num_classes=9)
+            }
+            output = model(sample)
+            output = output.clone().detach()
+            action = output[0].argmax().item()
+
+            _, reward, done, _ = env.step(action)
+            steps += 1
+
+            if done:
+                env.reset()
+                metric_success.append(reward > 0)
+                metric_steps.append(steps)
+    d = {
+        'success_rate': np.mean(metric_success),
+        'metric_steps': metric_steps
+    }
+    return d
+
+weights = [1., 1., 0.7, 0.01, 0.01, 0.01, 0.01, 0.01] # moving forward is quite likely
+class_weights = torch.FloatTensor(weights)
+cross_entropy = nn.CrossEntropyLoss()
+params = model.parameters()
+optimizer = optim.Adam(params, lr=args.lr)
+scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
+
+model_metrics = ModelMetrics(MODEL_NAME)
+
+model.train()
+for epoch in range(args.nb_epochs):
+    epoch_loss = []
+    for i, sample in enumerate(train_loader):
+        optimizer.zero_grad()
+        outputs = model(sample)
+        loss = cross_entropy(outputs, sample['a'].squeeze())
+        epoch_loss.append(loss.item())
+        loss.backward()
+        optimizer.step()
+    scheduler.step()
+    avg_loss = np.mean(epoch_loss)
+    print('[%d] loss: %.3f' % (epoch, avg_loss))
+    
+    model_metrics.add_loss(avg_loss)
+
+    if (epoch+1) % args.eval_epoch == 0:
+        model.eval()
+        eval_result = evaluateInEnv()
+        print('EVAL (success_rate) = ', eval_result['success_rate'])
+        model_metrics.add_eval(epoch, eval_result) 
+        model.train()
+
+
+model.eval()      
+eval_result = evaluateInEnv()
+model_metrics.add_eval(-1, eval_result) 
+
+torch.save({
+    'model_dict': model.state_dict()
+}, MODEL_PATH)
+
+with open(MODEL_METRICS_PATH, 'wb') as fp:
+    pickle.dump(model_metrics.getDict(), fp)
---
+++ agents/hist_dataset.py
@@ -0,0 +1,53 @@
+import os
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from glob import glob
+from core.utils import load_demo
+from PIL import Image
+
+class DemoDataPreviousAction(Dataset):
+    
+    def __init__(self, demo_folder, nb_demos, transform=None):
+        if transform:
+            self.transform = transform
+        else:
+            self.transform = lambda x : x
+
+        files = [f for f in glob(os.path.join(demo_folder, '*'))]
+        files = np.random.choice(files, nb_demos, False)
+        
+        self.obs = []
+        self.prev_a = []
+        self.a = []
+        for f in files:
+            d = load_demo(f)
+            for (o, prev_a, a) in zip(d.observations, [8] + d.actions[:-1], d.actions):
+                self.obs.append(Image.fromarray(o))
+                v = np.zeros(9, dtype=np.float32); v[prev_a] = 1.0
+                self.prev_a.append(v)
+                self.a.append(a)
+                prev_a = a
+        
+        self.prev_a = np.array(self.prev_a)
+        self.a = np.array(self.a, dtype=np.int64)[:, np.newaxis]
+
+    def __len__(self):
+        return len(self.a)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        if type(idx) is slice:
+            idx = list(range(len(self.obs)))[idx]
+        elif type(idx) is not list:
+            idx = [idx]
+        obs = torch.stack([self.transform(self.obs[i]) for i in idx])
+        prev_a = torch.from_numpy(self.prev_a[idx])
+        a = torch.from_numpy(self.a[idx])
+
+        return {
+            'obs': obs,
+            'prev_a': prev_a,
+            'a': a
+        }
