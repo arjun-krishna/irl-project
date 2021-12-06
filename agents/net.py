fb9edf678ecc9bbb8b8bd40e0f46580cb7243205
Arjun Krishna <arjunkris96@gmail.com>
2021-12-05 19:08:16 -0500

    Standardize network arch

--- agents/net.py
+++ agents/net.py
@@ -3,35 +3,57 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Encoder(nn.Module):
-    def __init__(self, dim=128) -> None:
+    def __init__(self) -> None:
         super().__init__()
-        # In: 3x50x50
-        self.conv1 = nn.Conv2d(3, 6, 7) # Out:   6x44x44
-        self.pool = nn.MaxPool2d(2, 2)  # Out:   6x22x22
-        self.conv2 = nn.Conv2d(6, 10, 7) # Out: 10x16x16
-        self.conv3 = nn.Conv2d(10, 16, 7) # Out: 16x10x10
-        self.fc1 = nn.Linear(16 * 10 * 10, 256)
-        self.fc2 = nn.Linear(256, dim)
+        # In: 3x64x64# In2: 3x256x256
+        self.blk1 = nn.Sequential(
+            nn.Conv2d(3, 32, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 64, 3, padding=1),  
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2, 2)          # 64x32x32 | 64x128x128
+        )
+        self.blk2 = nn.Sequential(
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(2, 2)          # 128x16x16 | 128x64x64
+        )
+        self.blk3 = nn.Sequential(
+            nn.Conv2d(128, 64, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2, 2)          # 64x8x8  | 64x32x32
+        )  
     
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
+        x = self.blk1(x)
+        x = self.blk2(x)
+        x = self.blk3(x)
         x = torch.flatten(x, 1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
         return x
 
 class MLP(nn.Module):
-    def __init__(self, dim=128) -> None:
+    def __init__(self, dim=128, p=0.4) -> None:
         super().__init__()
         # In: 120
-        self.fc1 = nn.Linear(dim, 64) # Out: 64
-        self.fc2 = nn.Linear(64, 32)  # Out: 32
-        self.fc3 = nn.Linear(32, 8)  # Out: 8
+        self.fc = nn.Sequential(
+            nn.Linear(dim, 64) # Out: 64
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=p),
+            nn.Linear(64, 32)  # Out: 32
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=p),
+            nn.Linear(32, 8)
+        )
     
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        return self.fc(x)
