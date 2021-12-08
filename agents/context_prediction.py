96763154909abdb90cb9a17e159eedadb3d275fc
Sanjeev Grampurohit <sanjeev15002@gmail.com>
2021-12-08 14:00:03 -0500

    metrics logging for context_prediction

--- agents/context_prediction.py
+++ agents/context_prediction.py
@@ -19,14 +19,13 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import pickle
 
-def train_model(model, train_loader, model_name: str,num_epochs: int = 20, lr=1e-4, save_path: str = './metrics/experiment.pickle', device=torch.device('cuda')):
-    loss_fn = nn.CrossEntropyLoss()
+def train_model(model, train_loader, loss_fn, optimizer, experiment_details,num_epochs: int = 20, eval_every=5, save_path: str = './metrics/experiment.pickle', device=torch.device('cuda')):
     if not os.path.isdir(os.path.dirname(save_path)):
         os.makedirs(os.path.dirname(save_path))
     logger = ModelMetrics(model_name)
     total_iters = 0
+    transform = experiment_details['transform']
     model.train()
-    optimizer = optim.Adam(params=model.parameters(), lr=lr)
     for epoch in range(num_epochs):
         avg_epoch_loss = 0
         for i, (input, label) in enumerate(train_loader):
@@ -82,14 +81,21 @@ def train_model(model, train_loader, model_name: str,num_epochs: int = 20, lr=1e
                 print('Loss = {}'.format(loss))
 
         avg_epoch_loss /= (i+1)
-        if (epoch+1)%num_epochs == 0:
-            model.eval
+        if (epoch+1-1)%num_epochs == 0:
+            eval_result = model.eval_in_env(experiment_details['env_name'], transform=transform, top_view=(experiment_details['view']=='top'))
+            logger.add_eval(epoch, eval_result) 
+            print('Success rate: ', eval_result['success_rate'], '    Steps: ', eval_result['metric_steps'])
+
         print(50*'=')
         print('Epoch {}: , Avg Loss: {}'.format(epoch, avg_epoch_loss))
         with open(save_path, 'wb') as f:
-            pickle.dump(logger.getDict(), f)
-        if (epoch+1) % args.eval_epoch == 0:
-            eval_result = evaluateInEnv()
+            d = logger.getDict()
+            d['experiment_details'] = experiment_details
+
+            pickle.dump(d, f)
+        if (epoch+1-1) % eval_every == 0:
+            eval_result = model.eval_in_env(experiment_details['env_name'], transform=transform, top_view=(experiment_details['view']=='top'))
+
             logger.add_eval(epoch, eval_result) 
 
 
@@ -101,11 +107,13 @@ if __name__ == '__main__':
     parser.add_argument('--num_epochs',default=20, type=int, help='number of epochs to train the model for')
     parser.add_argument('--lr',default=1e-4, type=float, help='learning rate for the model')
     parser.add_argument('--num_demos',default='all', type=int, help='number of demonstrations to use for training model. Use "all" (default) for all demos. Append "m" to integer to train with n_demos in multiples of  that integer. eg: "50m" trains the model with 50, 100, 150, 200, ... demos ')
-    parser.add_argument('--input_size', type=int, default=96, help='resize all input images to this size')
+    parser.add_argument('--input_size', type=int, default=224, help='resize all input images to this size')
     parser.add_argument('--batch_size', type=int, default=64, help='training batch size')
     parser.add_argument('--eval_every', type=int, default=5, help='evaluate after every "eval_every" epochs')
     parser.add_argument('--data_root', type=str, default='C:/Users/sanje/Documents/Projects/irl-project/demos/')
+    parser.add_argument('--weighted_ce_loss', action='store_true', help='Use a weighted cross entropy loss')
     parser.add_argument('--env_name', type=str, default='MiniWorld-Hallway-v0',help='Name of environment: MiniWorld-Hallway-v0 or MiniWorld-YMaze-v0 or MiniWorld-OneRoom-v0 or MiniWorld-FourRooms-v0')
+    parser.add_argument('--top_view', action='store_true', help='Switch to top view aka world view')
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
@@ -115,101 +123,26 @@ if __name__ == '__main__':
     model_name = 'ContextPredictionModel'
     model = ContextPredictionModel()
     view = 'top' if args.top_view else 'agent'
-    data_folder_path = os.path.join(args.dataroot, args.env_name, view)
+    data_folder_path = os.path.join(args.data_root, args.env_name, view)
     input_shape = (args.input_size, args.input_size)
-    train_dataset = ContextPredictionDataset(demo_folder = args.demo_folder, input_shape=input_shape, nb_demos=args.num_demos)
+    train_dataset = ContextPredictionDataset(demo_folder = data_folder_path, input_shape=input_shape, nb_demos=args.num_demos)
     print('len:',len(train_dataset))
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    weights = [1., 1., 0.1, 0.01, 0.01, 0.01, 0.01, 0.01] # moving forward is quite likely
+    class_weights = torch.FloatTensor(weights)
+    loss_fn = nn.CrossEntropyLoss(weight=class_weights) if args.weighted_ce_loss else nn.CrossEntropyLoss()
+    params = list(model.encoder.parameters()) + list(model.mlp_cp.parameters()) + list(model.mlp_bc.parameters())
+    optimizer = optim.Adam(params=params, lr=args.lr)
+
     experiment_details = {
         'model_name': model_name,
         'type': 'first/normal',
         'num_demos': args.num_demos,
+        'loss_fn': str(loss_fn), 
         'lr': args.lr,
         'view': view,
-        'Env':args.env_name
+        'env_name':args.env_name,
+        'transform': train_dataset.transform
     }
     save_path = './experiments/exp_' + str(time()) + '.pickle'
-    best_weights = train_model(model, train_loader, num_epochs=args.num_epochs, model_name=model_name, device = device, save_path=save_path)
-
-    def evaluateInEnv(model):
-        model.eval()
-        env = gym.make(args.env_name)
-        env.reset()
-        get_obs = env.render_top_view if args.top_view else env.render_obs
-
-        if args.domain_rand:
-            env.domain_rand = True
-
-        NUM_EPISODES = 100
-
-        metric_steps = []
-        metric_success = []
-
-        for i in range(NUM_EPISODES):
-            done = False
-            steps = 0
-            while not done:
-                x = Image.fromarray(get_obs())
-                inp = data_transform(x)
-                output = encoder(inp[np.newaxis, :, :, :])
-                output = mlp(output)
-                output = output.clone().detach()
-                action = output[0].argmax().item()
-
-                _, reward, done, _ = env.step(action)
-                steps += 1
-
-                if done:
-                    env.reset()
-                    metric_success.append(reward > 0)
-                    metric_steps.append(steps)
-        d = {
-            'success_rate': np.mean(metric_success),
-            'metric_steps': metric_steps
-        }
-        encoder.train()
-        mlp.train()
-        return d
-
-    weights = [1., 1., 0.1, 0.01, 0.01, 0.01, 0.01, 0.01] # moving forward is quite likely
-    class_weights = torch.FloatTensor(weights)
-    cross_entropy = nn.CrossEntropyLoss(weight=class_weights)
-    params = list(encoder.parameters()) + list(mlp.parameters())
-    optimizer = optim.Adam(params, lr=args.lr)
-
-    model_metrics = ModelMetrics(MODEL_NAME)
-
-    encoder.train()
-    mlp.train()
-
-    for epoch in range(args.nb_epochs):
-        epoch_loss = []
-        for i, (inputs, labels) in enumerate(train_loader):
-            optimizer.zero_grad()
-
-            outputs = encoder(inputs)
-            outputs = mlp(outputs)
-
-            loss = cross_entropy(outputs, labels)
-            epoch_loss.append(loss.item())
-            loss.backward()
-            optimizer.step()
-        avg_loss = np.mean(epoch_loss)
-        print('[%d] loss: %.3f' % (epoch, avg_loss))
-        
-        model_metrics.add_loss(avg_loss)
-
-        if (epoch+1) % args.eval_epoch == 0:
-            eval_result = evaluateInEnv()
-            model_metrics.add_eval(epoch, eval_result) 
-        
-    eval_result = evaluateInEnv()
-    model_metrics.add_eval(-1, eval_result) 
-
-    torch.save({
-        'encoder_dict': encoder.state_dict(),
-        'mlp_dict': mlp.state_dict()
-    }, MODEL_PATH)
-
-    with open(MODEL_METRICS_PATH, 'wb') as fp:
-        pickle.dump(model_metrics.getDict(), fp)
+    best_weights = train_model(model, train_loader, loss_fn, optimizer,experiment_details, num_epochs=args.num_epochs, eval_every=args.eval_every, device = device, save_path=save_path)
--- agents/models.py
+++ agents/models.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from agents.net import MLP, Encoder
+import gym
+import numpy as np
+from PIL import Image
 
 from torchvision import transforms as T
 
@@ -19,3 +22,47 @@ class ContextPredictionModel(nn.Module):
         features = torch.cat((features1, features2))
         out = self.mlp(features)
         return out 
+
+    def eval_in_env(self, env_name, transform, top_view=False, num_episodes=100, domain_rand=False):
+        self.encoder.eval()
+        self.mlp_bc.eval()
+        
+        env = gym.make(env_name)
+        env.reset()
+        get_obs = env.render_top_view if top_view else env.render_obs
+
+        if domain_rand:
+            env.domain_rand = True
+
+        metric_steps = []
+        metric_success = []
+
+        for i in range(num_episodes):
+            done = False
+            steps = 0
+            while not done and steps<=500:
+                x = Image.fromarray(get_obs())
+                prev_a = 8
+                v = torch.zeros(9, dtype=torch.float); v[prev_a] = 1.0
+                inp = transform(x)
+                output = self.encoder(inp[np.newaxis, :, :, :])
+                v = v[np.newaxis, :]
+                mlp_input = torch.cat((output, v), axis=1)
+                output = self.mlp_bc(mlp_input)
+                output = output.clone().detach()
+                action = output[0].argmax().item()
+
+                _, reward, done, _ = env.step(action)
+                steps += 1
+
+                if done:
+                    env.reset()
+                    metric_success.append(reward > 0)
+                    metric_steps.append(steps)
+        d = {
+            'success_rate': np.mean(metric_success),
+            'metric_steps': metric_steps
+        }
+        self.encoder.train()
+        self.mlp_bc.train()
+        return d
