a572e10dedddb9cbcc998378a5cd21a6441a2895
shstan <shstan@gmail.com>
2021-12-10 11:16:50 -0500

    added windows env

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,10 +1,12 @@
 from core.utils import create_image_dataset
 num_demos = [10, 50, 150, 200, 250]
+num_demos = [250]
 envs = ['MiniWorld-OneRoom-v0', 'MiniWorld-Hallway-v0', 'MiniWorld-YMaze-v0', 'MiniWorld-FourRooms-v0']
 seed = 42
+envs = ['MiniWorld-YMaze-v0']
 for env in envs:
     for demo in num_demos:
         create_image_dataset(env, 'agent', num_demos=demo, seed=seed)
-        if env != "MiniWorld-Hallway-v0":
+        if env != "MiniWorld-Hallway-v0" and False:
             create_image_dataset(env, 'top', num_demos=demo, seed=seed)
 print('done')
